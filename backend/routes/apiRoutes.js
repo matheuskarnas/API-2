b98e088ea2e565e2f0e29ed012dcbbd3bf144f11
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-03-18 09:46:18 -0300

    delete to use vite

--- backend/routes/apiRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/apiRoutes.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json({ message: 'API de exemplo' });
-});
-
-module.exports = router;
