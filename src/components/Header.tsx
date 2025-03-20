74c10c8bdb8352d7729f7dd1d642a5691043d775
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-03-20 12:22:08 -0300

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-white">
+      <nav className="mx-auto items-center justify-between p-10">
+      </nav>
+    </header>
+
+  );
+}
+
+export default Header
