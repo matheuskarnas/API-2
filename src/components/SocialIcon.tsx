f44895a7c80822529a7d73f05d81667f60f049b9
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-03-22 11:16:34 -0300

    feat: Add SocialIcon component for social media icons

---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,18 @@
+type SocialIconProps = {
+    ref: string,
+    img: string,
+    alt: string,
+ };
+
+
+export function SocialIcon({ref, img, alt}: SocialIconProps) {
+  return (
+    <a href={ref} target="_blank" rel="noopener noreferrer">
+      <img
+        src={`../../public/assets/${img}`}
+        alt={alt}
+        className="w-6 h-6"
+      />
+    </a>
+  );
+}
