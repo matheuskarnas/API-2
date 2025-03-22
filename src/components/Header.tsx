4feda8d338d5fe285767c41f1eae86cc5b1e201a
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-03-22 08:36:02 -0300

    feat: update Header component with new layout and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,27 @@
 function Header() {
   return (
-    <header className="bg-white">
-      <nav className="mx-auto items-center justify-between p-10">
-      </nav>
-    </header>
-
+    <nav className="w-full bg-blue-900 p-4 flex justify-between items-center">
+      <h1 className="text-white text-lg font-bold ml-4">Loggi</h1>
+      <div className="flex gap-3 mr-4">
+          <a href={'#'} target="_blank" rel="noopener noreferrer">
+            <img
+              src={"../../public/assets/logotype.enc"}
+              alt="Instagram"
+              className="w-6 h-6"
+            />
+          </a>
+        {/* {empresa.instagram && (
+          <a href={empresa.instagram} target="_blank" rel="noopener noreferrer">
+            <img
+              src="../assets/instagram.png"
+              alt="Instagram"
+              className="w-6 h-6"
+            />
+          </a>
+        )} */}
+      </div>
+    </nav>
   );
 }
 
-export default Header
+export default Header;
