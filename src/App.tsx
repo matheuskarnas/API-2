6180bc2a5aa27945739e243e1fb79a0c10c18d88
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-03-22 08:36:41 -0300

    feat: add EmpresaInfos and Stats components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { EmpresaInfos } from './components/EmpresaInfos.tsx'
 import Header from './components/Header.tsx'
+import { Stats } from './components/Stats.tsx'
 
 function App() {
   return (
     <>
       <Header/>
+      <EmpresaInfos/>
+      <Stats/>
     </>
   )
 }
