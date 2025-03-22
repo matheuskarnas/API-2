38903e415c416dbf5dcb859bb8039ca2029bc3d0
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-03-22 11:17:12 -0300

    feat: Update header to include dynamic social media icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,36 @@
+
+import {empresa} from '../../public/data/empresa.ts'
+import { SocialIcon } from './SocialIcon.tsx';
+
 function Header() {
   return (
     <nav className="w-full bg-blue-900 p-4 flex justify-between items-center">
-      <h1 className="text-white text-lg font-bold ml-4">Loggi</h1>
+      <h1 className="text-white text-lg font-bold ml-4">SHIP</h1>
       <div className="flex gap-3 mr-4">
-          <a href={'#'} target="_blank" rel="noopener noreferrer">
-            <img
-              src={"../../public/assets/logotype.enc"}
-              alt="Instagram"
-              className="w-6 h-6"
-            />
-          </a>
-        {/* {empresa.instagram && (
-          <a href={empresa.instagram} target="_blank" rel="noopener noreferrer">
-            <img
-              src="../assets/instagram.png"
-              alt="Instagram"
-              className="w-6 h-6"
-            />
-          </a>
-        )} */}
+        {empresa.facebook && (
+          <SocialIcon alt='facebook' img='facebook.png' ref={empresa.facebook}/>
+        )}
+        {empresa.instagram && (
+          <SocialIcon alt='instagram' img='instagram.webp' ref={empresa.instagram}/>
+        )}
+        {empresa.kawai && (
+         <SocialIcon alt='kawai' img='kawai.avif' ref={empresa.kawai}/>
+        )}
+        {empresa.linkedin && (
+          <SocialIcon alt='linkedin' img='linkedin.png' ref={empresa.linkedin}/>
+        )}
+        {empresa.site_web && (
+         <SocialIcon alt='site_web' img='web.png' ref={empresa.site_web}/>
+        )}
+        {empresa.tiktok && (
+          <SocialIcon alt='tiktok' img='tiktok.webp' ref={empresa.tiktok}/>
+        )}
+        {empresa.whatsapp && (
+          <SocialIcon alt='whatsapp' img='whatsapp.png' ref={empresa.whatsapp}/>
+        )}
+        {empresa.x && (
+          <SocialIcon alt='x' img='x.png' ref={empresa.x}/>
+        )}
       </div>
     </nav>
   );
