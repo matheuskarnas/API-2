43fb3722006cfd2135f24e339422d214f41744a6
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-03-22 11:15:42 -0300

    feat: Update company information with dynamic data

--- src/components/EmpresaInfos.tsx
+++ src/components/EmpresaInfos.tsx
@@ -1,15 +1,15 @@
+import { empresa } from "../../public/data/empresa";
+
 export function EmpresaInfos(){
     return(
         <div className="p-6 w-full max-w-md text-center bg-white rounded-lg mt-4">
         <img
-          src="/logo.png../../public/assets/logotype.enc"
+          src={`../../public/assets/${empresa.highSponsorLogo}`}
           alt="Loggi Logo"
           className="w-32 h-32 mx-auto p-2 rounded-md"
         />
         <p className="mt-4 text-gray-700">
-          A Loggi é uma startup brasileira de logística, fundada em 2013, que
-          usa tecnologia para otimizar entregas rápidas em cidades e regiões
-          metropolitanas.
+         {empresa.descriptionSponsor}
         </p>
       </div>
     )
