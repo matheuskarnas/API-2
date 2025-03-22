d028ed8427fe99d436906e4cda8fe70c825b58c2
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-03-22 11:14:07 -0300

    feat: Add company data to empresa.ts

---
+++ public/data/empresa.ts
@@ -0,0 +1,18 @@
+export const empresa = {
+    descriptionSponsor:
+      "Agência SHIP traça estratégias práticas para anunciar seu negócio online e encontrar clientes nas principais plataformas disponíveis para o Marketing Digital.",
+    descriptionTitle: "Conheça a Agencia Ship",
+    exclusiveUrl: "agenciaship",
+    facebook: "",
+    highSponsorLogo: "shipLogo.png",
+    instagram: "https://www.instagram.com/agenciaship/",
+    kawai: "",
+    linkedin: "",
+    lowSponsorLogo: "agenciaship_logo_1.webp",
+    nameSponsor: "Helpnei",
+    site_web: "https://agenciaship.com.br/",
+    tiktok: "",
+    urlSponsor: "https://unidos-helpnei.web.app/?s=agenciaship",
+    whatsapp: "https://api.whatsapp.com/send?phone=5512991235705",
+    x: "",
+  };
