17a87ec3523361253909d7e9ad459c8b9a42fd0d
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-03-26 19:22:38 -0300

    feat: Create route for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import { EmpresaPage } from './pages/EmpresaPage.tsx'
+import { Home } from './pages/Home.tsx'
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/empresa/:empresaId" element={<EmpresaPage />} />
       </Routes>
     </Router>
