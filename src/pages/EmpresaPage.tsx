b407bfab85df9dc02382c5eaa8b1b58cc460badc
LucasAraujo1016 <lucas.allbuquerque1234@gmail.com>
2025-03-27 18:02:54 -0300

    Feat: Add validation for empresaId and redirect to error page if invalid

--- src/pages/EmpresaPage.tsx
+++ src/pages/EmpresaPage.tsx
@@ -1,11 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { EmpresaInfos } from "../components/EmpresaInfos";
 import Header from "../components/Header";
 import Maps from "../components/Maps";
 import { Stats } from "../components/Stats";
+import { useEffect, useState } from "react";
 
 export function EmpresaPage() {
   const { empresaId } = useParams();
+  const navegarError = useNavigate();
+  const [empresaExiste, setEmpresaExiste] = useState<boolean>(true);
+
+  useEffect(() => {
+    const empresaIdValido = [1, 2, 3, 4, 5]; //Eventualmente será substituído por uma chamada do banco de dados
+    if (!empresaIdValido.includes(Number(empresaId))) {
+      setEmpresaExiste(false);
+      navegarError("/error");
+    }
+  }, [empresaId, navegarError]);
+
+  if (!empresaExiste) {
+    return null;
+  }
 
   console.log('empresaId', empresaId);
 
