386590fbc313181e199b707a7dcf26b7ded46131
LittleRob120 <gabriel360pam@gmail.com>
2025-04-11 09:10:15 -0300

    feat: add Card component with basic styling

---
+++ src/components/Card.tsx
@@ -0,0 +1,16 @@
+export const Card = () => {
+    return (
+        <div className="
+        bg-gray-100 p-3 rounded-lg shadow 
+        flex flex-col items-center
+        xl:w-[48%]
+        border-3 
+        border-[#328DD8]
+        shadow-md
+        shadow-black
+      ">
+        <p className="text-xs sm:text-sm md:text-base whitespace-nowrap">Dados</p>
+        <p className="text-xl sm:text-2xl md:text-3xl font-bold">{10}</p>
+      </div>
+    )
+}
