620c82ce3c4d9e5752a8d32260abd1f0777c5929
Lucas Guerra <lucasguerrawr2004@gmail.com>
2025-04-16 10:45:00 -0300

    feat: replace CadastroPage with CadastroUsuario and update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Home } from './pages/Home';
 import { EmpresaPage } from './pages/EmpresaPage';
 import { ErrorPage } from './pages/ErrorPage';
 import { Analytics } from '@vercel/analytics/react';
-import { CadastroPage } from './pages/CadastroPage';
+import { CadastroUsuario } from './pages/CadastroUsuario';
 import { CadastroEmpresas } from './pages/CadastroEmpresas';
 
 
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/empresa/:empresaUrl" element={<EmpresaPage />} />
-        <Route path="/usuario/cadastro" element={<CadastroPage/>} />
+        <Route path="/usuario/cadastro" element={<CadastroUsuario/>} />
         <Route path="*" element={<ErrorPage />} />
         <Route path="/empresa/cadastro" element={<CadastroEmpresas/>} />
       </Routes>
