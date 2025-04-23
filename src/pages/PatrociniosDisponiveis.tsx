2992f26a8d2cc14b1b09476cb40ef1a67321f1c1
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-04-23 12:06:51 -0300

    refactor: remove unused imports in PatrociniosDisponiveis component

--- src/pages/PatrociniosDisponiveis.tsx
+++ src/pages/PatrociniosDisponiveis.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import { supabase } from "../services/supabaseClient";
+import { useState } from "react";
 import Header from "../components/Header";
 import { Link, useLocation } from "react-router-dom";
 
