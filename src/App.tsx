bccaed1d56f6862d5535afc26e87c12f7cb56fb9
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-04-26 19:34:23 -0300

    feat: Add UsuarioPlanos component and update routing in App to include user plans

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { CadastroUsuario } from './pages/CadastroUsuario';
 import { CadastroEmpresas } from './pages/CadastroEmpresas';
 import { Patrocinio } from './pages/Patrocinio';
 import { PatrociniosDisponiveis } from "./pages/PatrociniosDisponiveis";
+import { UsuarioPlanos } from "./pages/UsuarioPlanos";
 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
         <Route path="/empresa/planos" element={<ErrorPage />} /> // criar página de planos para empresas
         <Route path="/empresa/cadastro" element={<CadastroEmpresas/>} />
         <Route path="/empresa/patrocinio" element={<Patrocinio/>} />
-        <Route path="/usuario/planos" element={<ErrorPage/>} /> // criar página de planos para usuários
+        <Route path="/usuario/planos" element={<UsuarioPlanos/>} /> // criar página de planos para usuários
         <Route path="/usuario/cadastro" element={<CadastroUsuario/>} />
         <Route path="/usuario/patrocinios-disponiveis" element={<PatrociniosDisponiveis/>} />
         <Route path="*" element={<ErrorPage />} />
