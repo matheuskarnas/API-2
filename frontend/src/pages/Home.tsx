983a1a94f8a66c231361177c3c96e406c11edd0a
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-05-23 10:14:42 -0300

    feat: Add new Link component for Vendedores in Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -174,6 +174,7 @@ export function Home() {
             Planos para Empresas
           </Link>
           <Link to="" className="bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1">
+            Planos para Vendedores
           </Link>
         </div>
 
